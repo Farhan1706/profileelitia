1285d56fe413fa5f0e570a13590fe0555f5ff435
cinsriastiti <cindy.sriastiti25@gmail.com>
2025-05-01 19:46:26 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -22,6 +22,8 @@ func MapRoutes(server *http.ServeMux, db *sql.DB) {
     r.HandleFunc("/divisi/tambah", controller.AddDivisi())
     r.HandleFunc("/divisi/sunting/{id}", controller.EditDivisi(db))
     r.HandleFunc("/divisi/tambah/proses", controller.InsertDivisi(db))
+    r.HandleFunc("/edit_deskripsi", controller.EditDeskripsiForm(db))
+    r.HandleFunc("/update_deskripsi", controller.UpdateDeskripsi(db))
 
     // file statis
     r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
